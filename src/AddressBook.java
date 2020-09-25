c2e4a158c55be9c26a835c3d6d5bb4230ae25c0f
TylerLeung <leung.tyler008@gmail.com>
2020-09-25 01:49:07 -0400

    added AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -15,7 +15,7 @@ public class AddressBook {
     }
 
     public static void main (String[] args) {
-        BuddyInfo buddy = new BuddyInfo("Tom","Carleton", "613");
+        BuddyInfo buddy = new BuddyInfo("Jerry","Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
