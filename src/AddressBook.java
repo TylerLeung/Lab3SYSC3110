ac684e2af167189a141b507f3bd065f802ff7085
TylerLeung <leung.tyler008@gmail.com>
2020-10-02 02:07:15 -0400

    commit to new branch

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -14,12 +14,17 @@ public class AddressBook {
         buddyList.remove(buddy);
     }
 
+    public ArrayList<BuddyInfo> getBuddyList() {
+        return buddyList;
+    }
+
     public static void main (String[] args) {
         BuddyInfo buddy = new BuddyInfo("Jerry","Carleton", "613");
         BuddyInfo buddy2 = new BuddyInfo("Tom","Carleton","613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.addBuddy(buddy2);
+        System.out.println(addressBook.getBuddyList());
         addressBook.removeBuddy(buddy);
     }
 }
